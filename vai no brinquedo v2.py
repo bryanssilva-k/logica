b3512b67644f7f4d91c8f7c1c2526806ee8cc5b1
bryan_silva <bryan.s.silva@edu.senai.br>
2026-03-23 10:06:43 -0300

    Add files via upload

---
+++ vai no brinquedo v2.py
@@ -0,0 +1,7 @@
+altura = float(input("Digite sua altura: "))
+problema = input("Tem problema cardíaco Sim/não: ")
+
+if altura > 1.5 and problema == "não":
+    print("Vai no brinquedo")
+else:
+    print("não vai no brinquedo")
